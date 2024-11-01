8458b137324c8df5841c5b39fdba846cf3a24123
JoySuhas <144987065+JoySuhasChandolu@users.noreply.github.com>
2024-11-01 14:38:00 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1212315
+let accountEmail = "reiui@gmail.com"
+var accountPassword = "12342"
+accountCity = "Jaipur"
+
+console.log();
---
+++ 02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("hello"); we are using node js not browser
+
+console.log(3+4); 
+
+//PRIMITIVE DATA TYPES
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//NON PRIMITIVE 
+
+//object
+
+console.log(typeof null);

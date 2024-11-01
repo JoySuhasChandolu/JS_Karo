d602042869ca7c2344288c8b193d9821431adbbb
JoySuhas <144987065+JoySuhasChandolu@users.noreply.github.com>
2024-11-01 14:52:15 +0530

    Add files via upload

---
+++ 03_conversion.js
@@ -0,0 +1,23 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber =  Number(score);
+
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33Abc" => NaN
+// true => 1;false => 0
+
+let isLoggedIn = null;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "jore" => true
